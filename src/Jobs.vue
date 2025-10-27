b2acea637e1fa54d02ec960b2791e8c8cd2116a5
chomax21 <maximcheb21@gmail.com>
2025-10-27 22:40:44 +0300

    feat: Доработает загрузу дел, их отображение и drag and drop

--- src/Jobs.vue
+++ src/Jobs.vue
@@ -2,18 +2,14 @@
 import { ref, reactive, onMounted } from 'vue'
 import api from '@/services/api'
 import Cookies from 'js-cookie'
+import draggable from 'vuedraggable'
 
-const form = reactive({
-    login: ''
-})
-
-const jobList = ref([[]])
 const testData = ref(null)
 const login = ref(false)
 const error = ref(null)
 const message = ref(null)
-const data = ref(null);
-
+const data = ref(null)
+const jobs = ref([])
 
 onMounted(async () => {
     try {
@@ -26,7 +22,8 @@ onMounted(async () => {
                 'api/jobs', {}
             )
             if (response.status == 200) {
-                testData.value = response.data
+                jobs.value = JSON.parse(JSON.stringify(response.data))
+                console.log(jobs.value)
             }
         }
     } catch (err) {
@@ -35,7 +32,7 @@ onMounted(async () => {
     }
 })
 
-async function logout(){
+async function logout() {
     Cookies.remove('userData')
     router.push('/')
 }
@@ -43,23 +40,50 @@ async function logout(){
 
 <template>
     <div class="container-fluid">
-        <div class="position-absolute top-50 start-50 translate-middle">
-            <div class="d-flex">
-                <div class="col">
-                    <h3>СПИСОК ЗАДАЧ!</h3>
-                </div>                
+        <div class="position-relative py-2 px-4 shadow border-3 rounded mt-5">
+            <div class="d-flex justify-content-center align-items-center">
+                <draggable v-model="jobs" item-key="id">
+                    <template #item="{ element }">
+                        <div class="border border-3 rounded m-1 p-2">
+                            <p>
+                                <strong>Заголовок: </strong> {{ element.title }}                                
+                            </p>
+                            <p>
+                                 <strong>Описание: </strong>{{ element.description }}                                
+                            </p>
+                            <p class="p-0 m-0">
+                                 <strong>Статус:</strong> {{ element.status }}                                
+                            </p>
+                            <p class="p-0 m-0">
+                                 <strong>Создано: </strong> {{ element.createdAt }}                                
+                            </p>
+                            <p class="p-0 m-0">
+                                 <strong>Обновлено: </strong> {{ element.updatedAt }}                                
+                            </p>
+                            <p class="p-0 m-0">
+                                 <strong>Создатель: </strong> {{ element.createdBy.login }}                                
+                            </p>
+                            <p class="p-0 m-0">
+                                 <strong>Назначена: </strong> {{ element.assignedTo.login }}                                
+                            </p>
+                        </div>                        
+                    </template>
+                </draggable>
             </div>
+        </div>
+        <div class="position-relative py-2 px-4 mt-5">
+             <div class="col">
             <div class="d-flex">
-                <div class="d-flex mt-3 justify-content-center">
-                    <!-- <button type="submit" class="btn btn-success shadow">Создать</button> -->
-                    <router-link class="btn btn-success shadow" to="/create-jobs">Создать</router-link>
+                <div class="col align-self-center offset-md-5">
+                    <div class="d-flex mt-3 ">
+                        <router-link class="btn btn-success shadow" to="/create-jobs">Добавить новую
+                            задачу</router-link>
+                    </div>
+                    <div class="d-flex mt-3 ms-5 ps-4">
+                        <a v-on:click="logout" href="/">Выйти</a>
+                    </div>
                 </div>
             </div>
-            <div class="d-flex">
-                <div class="d-flex mt-3 justify-content-center">
-                    <!-- <router-link to="/">Назад</router-link> -->
-                     <a v-on:click="logout" href="/">Выйти</a>
-                </div>
             </div>
         </div>
     </div>
