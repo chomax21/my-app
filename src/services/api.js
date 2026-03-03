a25872b064d18b2fbd8a9acba9fce8206c043d6b
chomax21 <maximcheb21@gmail.com>
2026-03-03 22:09:59 +0300

    chore: Заменит строку подключения

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  //baseURL: 'http://localhost:5000/',
-  baseURL: 'http://localhost:5206/',
+  baseURL: 'http://localhost:5000/',
+  //baseURL: 'http://localhost:5206/',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
